c57715f8d9095afc23d62e9126f2254ee6a65a42
brunoguerra <brunoguerra@hotmail.com>
2024-04-05 09:27:23 -0300

    first commit

---
+++ src/main/java/br/com/fiap/mspedido/MspedidoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mspedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MspedidoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MspedidoApplication.class, args);
+    }
+
+}
